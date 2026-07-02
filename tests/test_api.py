032a89bcc04bffbf80eeb540ce9127fd7211a0bd
Ccode-400 <stanleymngige@gmail.com>
2026-07-03 00:38:14 +0300

    added home route test

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,3 +6,9 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
+def test_home(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert data["message"] == "Inventory Management API"
