d14c6bece4c7cd083a891f0da457ab2ffb3ee757
Ccode-400 <stanleymngige@gmail.com>
2026-07-02 21:50:59 +0300

    added inventory dictionary list

---
+++ inventory.py
@@ -0,0 +1,29 @@
+inventory = [
+    {
+        "id": 1,
+        "barcode": "737628064502",
+        "product_name": "Organic Almond Milk",
+        "brand": "Silk",
+        "price": 4.50,
+        "stock": 20,
+        "category": "Beverages"
+    },
+    {
+        "id": 2,
+        "barcode": "3017620422003",
+        "product_name": "Nutella",
+        "brand": "Ferrero",
+        "price": 6.99,
+        "stock": 15,
+        "category": "Spread"
+    },
+    {
+        "id": 3,
+        "barcode": "5449000000996",
+        "product_name": "Coca-Cola",
+        "brand": "Coca-Cola",
+        "price": 2.50,
+        "stock": 30,
+        "category": "Soft Drinks"
+    }
+]
