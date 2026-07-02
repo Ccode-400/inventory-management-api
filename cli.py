f0df2e0feed2a3eabb50cdebb5351ffbd041602f
Ccode-400 <stanleymngige@gmail.com>
2026-07-02 23:39:18 +0300

    added view_inventory function

--- cli.py
+++ cli.py
@@ -1,3 +1,30 @@
 import requests
 
 BASE_URL = "http://127.0.0.1:5000"
+
+def view_inventory():
+    response = requests.get(f"{BASE_URL}/inventory")
+
+    if response.status_code == 200:
+        items = response.json()
+
+        if not items:
+            print("\nInventory is empty.\n")
+            return
+
+        print("\n===== INVENTORY =====")
+
+        for item in items:
+            print(f"""
+ID: {item['id']}
+Name: {item['product_name']}
+Brand: {item['brand']}
+Price: ${item['price']}
+Stock: {item['stock']}
+Category: {item['category']}
+Barcode: {item['barcode']}
+-----------------------------
+""")
+    else:
+        print("Error retrieving inventory.")
+
