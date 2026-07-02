8590f299add484318c9b36a5077ea185a9bef240
Ccode-400 <stanleymngige@gmail.com>
2026-07-03 00:39:38 +0300

    added get all inventory test

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,10 @@ def test_home(client):
     assert response.status_code == 200
     data = response.get_json()
     assert data["message"] == "Inventory Management API"
+
+def test_get_inventory(client):
+    response = client.get("/inventory")
+    assert response.status_code == 200
+    data = response.get_json()
+    assert isinstance(data, list)
+    assert len(data) >= 1
