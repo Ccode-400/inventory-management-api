02316484e6e8d712f6d743f4c27c84f7c7c6048a
Ccode-400 <stanleymngige@gmail.com>
2026-07-02 22:44:15 +0300

    added fetch_product function which fetches using barcode

--- external_api.py
+++ external_api.py
@@ -1,3 +1,18 @@
 import requests
 
 BASE_URL = "https://world.openfoodfacts.org/api/v0/product"
+
+def fetch_product(barcode):
+    """
+    Fetch product details from OpenFoodFacts using a barcode.
+
+    Args:
+        barcode (str): Product barcode.
+
+    Returns:
+        dict: Product information if found.
+        None: If product does not exist or request fails.
+    """
+
+    url = f"{BASE_URL}/{barcode}.json"
+
