3f84caefa3dc938bd49755deccbd86d46c9a29a2
Ccode-400 <stanleymngige@gmail.com>
2026-07-03 11:16:22 +0300

    added inventorylist function

--- frontend/src/components/InventoryList.jsx
+++ frontend/src/components/InventoryList.jsx
@@ -1,1 +1,18 @@
 import api from "../services/api";
+
+function InventoryList({ inventory, refresh }) {
+    const deleteItem = async (id) => {
+        const confirmDelete = window.confirm(
+            "Are you sure you want to delete this item?"
+        );
+
+        if (!confirmDelete) return;
+        try {
+            await api.delete(`/inventory/${id}`);
+            alert("Item deleted successfully!");
+            refresh();
+        } catch (error) {
+            console.error(error);
+            alert("Failed to delete item.");
+        }
+    };
